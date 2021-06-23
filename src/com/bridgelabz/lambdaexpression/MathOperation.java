80e79fde35e60416628fde9703dea0535f2e1517
Roshni1998 <roshnimali847@gmail.com>
2021-06-23 15:38:52 +0530

    Welcome Message

---
+++ src/com/bridgelabz/lambdaexpression/MathOperation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.lambdaexpression;
+
+public class MathOperation {
+    public static void main(String[] args)   {
+        System.out.println("Welcome to Functional Programming using Lambda Expression");
+    }
+}
