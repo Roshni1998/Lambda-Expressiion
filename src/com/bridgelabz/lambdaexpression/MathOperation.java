fb96c375cb8430270bfe23f6bfc7cbab4534e18d
Roshni1998 <roshnimali847@gmail.com>
2021-06-23 16:05:02 +0530

    Math Opreation UC-1.2

--- src/com/bridgelabz/lambdaexpression/MathOperation.java
+++ src/com/bridgelabz/lambdaexpression/MathOperation.java
@@ -3,22 +3,25 @@ package com.bridgelabz.lambdaexpression;
 @FunctionalInterface
 interface IMathFunction{
     int calculate(int a, int b);
+    static void printResult(int a, int b, String function, IMathFunction fobj){
+        System.out.println("Result of " + function+ "is"+fobj.calculate(a, b));
+    }
 }
 
 public class MathOperation {
-    public static void main(String[] args)   {
+    public static void main(String[] args) {
         //Using Method Reference instead of Lambda Expression
         //This expression implements 'IMathFunction' interface
         IMathFunction add = Integer::sum;
 
         //Lambda Expression for multiplication and division for 2 parameters
         //This expression implements 'IMathFunction'
-        IMathFunction multiply = (x, y) -> x*y;
-        IMathFunction divide = (int x, int y) -> x/y;
+        IMathFunction multiply = (x, y) -> x * y;
+        IMathFunction divide = (int x, int y) -> x / y;
 
-        //Add & Multiply two numbers using Lambda Expression
-        System.out.println("Addition is : "+ add.calculate(6, 3));
-        System.out.println("Multiplication is : "+multiply.calculate(5, 7));
-        System.out.println("Division is : "+divide.calculate(35, 5));
+        //Passing Lambda Expression as Function Parameter to Print Result using Static Method
+        IMathFunction.printResult(4, 6, "Addition", add);
+        IMathFunction.printResult(5, 3, "Multiplication", multiply);
+        IMathFunction.printResult(24, 6, "Division", divide);
     }
 }
